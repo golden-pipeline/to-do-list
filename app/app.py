1ae22fd0db225c7a742e1f9cead7a186897855d1
Nicolas Castro <nicolas_castro123@hotmail.com>
2025-03-04 11:30:11 -0300

    Melhorando aplicação

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
+from prometheus_flask_exporter import PrometheusMetrics
 from flask import Flask
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
-@app.route("/")
-def home():
+# Exemplo de métrica personalizada
+metrics.info('app_info', 'Application info', version='1.0.0')
+
+@app.route('/')
+def hello():
     return "Hello, DevOps v1.0.0"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
