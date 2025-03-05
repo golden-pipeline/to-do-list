ad11e538ad01b3d592275aacaf73632d1c18c0e2
Nicolas Castro <nicolas_castro123@hotmail.com>
2025-03-05 16:21:30 -0300

    Implementando novas métricas na aplicação

--- app/app.py
+++ app/app.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
+import psutil
 
 app = Flask(__name__)
-
-# Inicia as métricas
 metrics = PrometheusMetrics(app)
 
-# Registra a métrica app_info diretamente
 metrics.info('app_info', 'Application info', version='1.0.0')
 
 @app.route('/')
 def hello():
     return "Hello, DevOps v1.0.0"
+
+#Métircas de CPU e Memória.
+@metrics.gauge('cpu_percent', 'CPU usage percent')
+def cpu_percent():
+    return psutil.cpu_percent()
+
+@metrics.gauge('memory_percent', 'Memory usage percent')
+def memory_percent():
+    return psutil.virtual_memory().percent
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
